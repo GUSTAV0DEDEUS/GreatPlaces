6e6f3171db7b7ae7602fb7b6020e12ea3e866c28
GUSTAV0DEDEUS <gustavodedeusconceicao@gmail.com.br>
2023-12-18 12:57:37 -0300

    :sparkles: feat(widget): Create image input

---
+++ lib/widgets/image_input.dart
@@ -0,0 +1,66 @@
+import 'dart:io';
+
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+
+class ImageInput extends StatefulWidget {
+  const ImageInput({super.key});
+
+  @override
+  State<ImageInput> createState() => _ImageInputState();
+}
+
+class _ImageInputState extends State<ImageInput> {
+  File? _storedImage;
+  _takePhoto() async {
+    final ImagePicker _picker = ImagePicker();
+    XFile imageFile = await _picker.pickImage(
+      source:
+          kIsWeb || Platform.isLinux || Platform.isWindows || Platform.isMacOS
+              ? ImageSource.gallery
+              : ImageSource.camera,
+      maxWidth: 600,
+    ) as XFile;
+
+    setState(() {
+      _storedImage = File(imageFile.path);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Container(
+          width: 180,
+          height: 100,
+          decoration: BoxDecoration(
+            border: Border.all(
+              width: 1,
+              color: Colors.grey,
+            ),
+          ),
+          alignment: Alignment.center,
+          child: _storedImage != null
+              ? Image.file(
+                  _storedImage!,
+                  width: double.infinity,
+                  fit: BoxFit.cover,
+                )
+              : Text('There are no images'),
+        ),
+        SizedBox(width: 10),
+        Expanded(
+          child: TextButton.icon(
+            onPressed: () {
+              _takePhoto();
+            },
+            icon: Icon(Icons.camera),
+            label: Text('Take a photo'),
+          ),
+        ),
+      ],
+    );
+  }
+}
